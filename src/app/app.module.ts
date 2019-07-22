8a2a1e1d1a3c8c8c2cfe934acbafc57a645c8a82
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-07-22 23:49:33 +0530

    Formatted and added toolbar.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatTabsModule } from '@angular/material';
+import { MatButtonModule, MatTabsModule, MatToolbarModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,6 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     MatButtonModule,
     MatTabsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
